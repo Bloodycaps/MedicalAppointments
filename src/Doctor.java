1f1585ce60e92306505d41e7a0a22b4e524fae58
Danilo Cortes <dacortes63@misena.edu.co>
2020-05-18 18:52:02 -0500

    chore: se inicia le proyecto para generar citas medicas

---
+++ src/Doctor.java
@@ -0,0 +1,19 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del doctor es: "+ name);
+    }
+
+    //Comportamientos
+    public void showName() {
+        System.out.println(name);
+    }
+
+}
