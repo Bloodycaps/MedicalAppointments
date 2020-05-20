26271b58f284256b8e8023cbca24fdee254b4abe
Danilo Cortes <dacortes63@misena.edu.co>
2020-05-20 16:21:29 -0500

    chore: se crea la logica con la interfaz de UI

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,19 +1,24 @@
 public class Doctor {
-    int id;
+    //Atributos
+    static int id = 0;//autoincrement
     String name;
     String speciality;
 
-    Doctor(){
+    Doctor() {
         System.out.println("Construyendo el objeto Doctor");
+        id++;
     }
 
-    Doctor(String name){
-        System.out.println("El nombre del doctor es: "+ name);
+    Doctor(String name) {
+        System.out.println("El nombre del doctor es: " + name);
     }
 
     //Comportamientos
     public void showName() {
         System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("Id doctor: "+ id);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Danilo");
         myDoctor.name = "David Fagua";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor myDoctorDave = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
     }
 }
