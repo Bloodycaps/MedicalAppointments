952fc7783e4082fad82038ad3a45be2a1129bcaa
Danilo Cortes <dacortes63@misena.edu.co>
2020-05-28 17:51:22 -0500

    chore: se realiza la clase estatica interna a la clase Doctor para mostras las citas disponibles;

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     //Atributos
     static int id = 0;//autoincrement
-    String name;
-    String speciality;
+    private String name;
+    private String speciality;
 
     Doctor() {
         System.out.println("Construyendo el objeto Doctor");
@@ -21,6 +24,51 @@ public class Doctor {
     }
     public void showId(){
         System.out.println("Id doctor: "+ id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private  String time;
+
+        public AvailableAppointment(Date date, String time){
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         //showMenu();
         Doctor alejandro = new Doctor("Alejandro", "Veterinario");
+
+        alejandro.addAvailableAppointment(new Date(), "16:00");
+        alejandro.addAvailableAppointment(new Date(), "12:00");
+        alejandro.addAvailableAppointment(new Date(), "1:00");
+
+        for (Doctor.AvailableAppointment aA :
+                alejandro.getAvailableAppointments()) {
+            System.out.println(aA.getDate().toLocaleString() + " " + aA.getTime());
+        }
+
+        /*
         System.out.println(alejandro.name);
         System.out.println(alejandro.speciality);
 
@@ -11,6 +24,6 @@ public class Main {
 
         patient.setHeight(54.6);
         System.out.println(patient.getHeight());
-
+        */
     }
 }
